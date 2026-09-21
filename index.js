6cfd35270072f05052ef16d5c9acf9d86f24da92
vikyascode <vikash@wespeakcode.com>
2026-09-22 00:24:37 +0530

    runner added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express'
 const app= express()
 
 app.get('/',(req,res)=>{
-    res.send("your server is healthy and working okay for now")
+    res.send("your server is healthy ")
 })
 
 app.listen(3000,()=>{
