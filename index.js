b410b35522ff5e63d5a03d84b6cdaf00ce7f44a1
vikyascode <vikash@wespeakcode.com>
2026-09-21 22:53:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+
+const app= express()
+
+app.get('/',(req,res)=>{
+    res.send("your server is healthy")
+})
+
+app.listen(3000,()=>{
+    console.log('your server is listing on 3000')
+})
